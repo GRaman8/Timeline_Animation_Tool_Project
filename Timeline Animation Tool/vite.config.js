d0ca3ddc7eb8e9925264374979be3c2894294a93
GRaman8 <ganapathiramanaz1@gmail.com>
2026-02-01 21:17:39 -0500

    Made changes to the vite.config file

--- Timeline Animation Tool/vite.config.js
+++ Timeline Animation Tool/vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import process from 'process'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: "",
-  },
 })
